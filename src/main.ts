8ed0fa42934f33a7b7bea19be56db73f0e6230fb
kvanarsd <kvanarsd@ucsc.edu>
2024-10-05 20:17:53 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My katrina game";
+const gameName = "My Fairy game";
 document.title = gameName;
 
 const header = document.createElement("h1");
@@ -13,4 +13,17 @@ app.append(header);
 const button = document.createElement('button');
 button.textContent = "🧚🏻";
 
+// counter
+let counter = 0;
+
+button.addEventListener('click', () => {
+    counter++;
+    dispCounter.textContent = counter + " Fairy Dust";
+});
+
 app.appendChild(button);
+
+// div
+const dispCounter = document.createElement('div');
+dispCounter.textContent = counter + " Fairy Dust";
+app.appendChild(dispCounter);
